ffef5f296f141129bb533da58a75ccf56f476f2a
gnithin <nithin.linkin@gmail.com>
2016-06-30 01:17:22 +0530

    [clisp] Section 1 in LOL

---
+++ clisp/lol/guess-number.lisp
@@ -0,0 +1,48 @@
+;;; Guessing game
+
+
+(defparameter *small* 1)
+(defparameter *big* 100)
+
+(defun guess-number()
+    (ash (+ *small* *big*) -1)
+)
+
+(defun smaller()
+     (setf *big* (- (guess-number) 1))
+     (guess-number)
+
+)
+(defun bigger()
+     (setf *small* (+ (guess-number) 1))
+     (guess-number)
+)
+(defun start-over()
+    (setf *small* 1)
+    (setf *big* 100)
+    (guess-number)
+)
+
+;; Trying out nested functions
+(defun trial(n)
+    (let 
+        (
+            (temp1 100)
+            (temp2 200)
+        )
+        (labels (
+                    (
+                     println(s)
+                         (print s)
+                     )
+                   (display_contents()
+                    (println temp1)
+                    (println temp2)
+                    (println n)
+                    (- 20 n)
+                   )
+              )
+          (display_contents)
+        )
+    )
+)
