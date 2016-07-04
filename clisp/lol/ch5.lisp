de5ae6048351168711168d52068b3957cc4a794d
gnithin <nithin.linkin@gmail.com>
2016-07-04 22:42:42 +0530

    [clisp] Add ch5 wizard game

--- clisp/lol/ch5.lisp
+++ clisp/lol/ch5.lisp
@@ -9,6 +9,30 @@
     )
 )
 
+(defparameter *edges* 
+    '(
+        (living-room (garden west door)
+            (attic upstairs ladder))
+        (garden (living-room east door))
+        (attic (living-room downstairs ladder))
+    )
+)
+
+;; Listing all visible objects and their locations
+(defparameter *objects* '(whiskey bucket frog chain))
+(defparameter *object-locations '(
+    (whiskey living-room)
+    (bucket living-room)
+    (frog garden)
+    (chain garden)
+))
+
+;; Taking a look around
+(defparameter *location* 'living-room)
+
+;; User inventory
+(defparameter *user-inventory* nil)
+
 ;; Method for getting the correct description
 (defun describe-location(location container)
     ;; This is setting a global value.
@@ -19,15 +43,6 @@
     (cadr location-value)
     ;; This could also be simply done as 
     ;; (cadr (assoc location container))
-)
-
-(defparameter *edges* 
-    '(
-        (living-room (garden west door)
-            (attic upstairs ladder))
-        (garden (living-room east door))
-        (attic (living-room downstairs ladder))
-    )
 )
 
 (defun describe-path-from(from to container)
@@ -44,15 +59,6 @@
         )
     )
 )
-
-;; Listing all visible objects and their locations
-(defparameter *objects* '(whiskey bucket frog chain))
-(defparameter *object-locations '(
-    (whiskey living-room)
-    (bucket living-room)
-    (frog garden)
-    (chain garden)
-))
 
 (defun objects-at(loc obj obj-loc)
     (labels 
@@ -86,18 +92,12 @@
     )
 )
 
-;; Taking a look around
-(defparameter *location* 'living-room)
-
 (defun look()
     (append 
         (describe-location *location* *nodes*)
         (describe-all-paths *location* *edges*)
         (describe-objects *location* *objects* *object-locations)
         ))
-
-;; Making it all work
-(look)
 
 ;; Moving it
 (defun walk (dirn)
@@ -120,3 +120,28 @@
         )
     )
 )
+
+;; Picking things up
+(defun pickup-stuff(obj)
+    ;; Check if the object exists in the current location
+    ;; Then remove the things and put it in the inventory
+        (if (find obj *objects*)
+            (progn
+                (cond
+                    ((eq (cadr (assoc obj *object-locations)) *location*)
+                            (if (not (find obj *user-inventory*))
+                                (setf *user-inventory* (append *user-inventory* (list obj)))
+                                *user-inventory*
+                                )
+                        )
+                    (t '(Such an object is not available in the room))
+                    )
+                
+            )
+            '(Such an object does not exist)
+        )
+    )
+
+;; Checking inventory
+(defun check-inventory()
+    (append '(items-) *user-inventory*))
