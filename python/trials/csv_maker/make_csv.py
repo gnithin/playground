b2958005a74344157b6384c7725b481892062e31
gnithin <nithin.linkin@gmail.com>
2016-09-20 15:06:19 +0530

    [python] Add changes to the csv maker

--- python/trials/csv_maker/make_csv.py
+++ python/trials/csv_maker/make_csv.py
@@ -1,6 +1,12 @@
+
+
 def make_n_entries_for_csv_filename(filename,
-                                    start_id=20001, num_entries=10000):
+                                    start_id=20001,
+                                    num_entries=10000,
+                                    heading_row=True
+                                    ):
     output_csv_li = []
+    output_csv_li.append("BigLT_All_IDs,BigLT_All_Passwords")
     for i in range(start_id, start_id + num_entries):
         next_str = str(i)
         email_id = "ltuser+" + next_str + "+i@enfold.com"
@@ -8,9 +14,11 @@ def make_n_entries_for_csv_filename(filename,
         output_csv_li.append(",".join([email_id, password]))
 
     with open(filename, 'w') as fw:
-        fw.write("\n".join(output_csv_li))
+        fw.write("\r\n".join(output_csv_li))
 
 if __name__ == "__main__":
+    num_entries = 50000
     make_n_entries_for_csv_filename(
-        "10k_users.csv"
+        str(num_entries) + "_users.csv",
+        num_entries=num_entries
     )
