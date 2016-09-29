505f46d1b958d947e7fe0184f0b13e675a817122
gnithin <nithin.linkin@gmail.com>
2016-09-29 19:34:19 +0530

    [python] Add file limits

--- python/trials/csv_maker/make_csv.py
+++ python/trials/csv_maker/make_csv.py
@@ -1,4 +1,5 @@
-
+l_users_num = 10
+m_users_num = 500
 
 def make_n_entries_for_csv_filename(filename,
                                     start_id=20001,
@@ -7,17 +8,21 @@ def make_n_entries_for_csv_filename(filename,
                                     ):
     output_csv_li = []
     output_csv_li.append("BigLT_All_IDs,BigLT_All_Passwords")
+
+    # Sprinkle medium and large users too from time to time
+
     for i in range(start_id, start_id + num_entries):
         next_str = str(i)
         email_id = "ltuser+" + next_str + "+i@enfold.com"
         password = "Enfold*" + next_str
         output_csv_li.append(",".join([email_id, password]))
 
+
     with open(filename, 'w') as fw:
         fw.write("\r\n".join(output_csv_li))
 
 if __name__ == "__main__":
-    num_entries = 50000
+    num_entries = 100000
     make_n_entries_for_csv_filename(
         str(num_entries) + "_users.csv",
         num_entries=num_entries
