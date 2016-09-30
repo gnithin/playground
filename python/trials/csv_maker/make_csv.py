8c754d257671cf35fc0503d5deb15406b4cac48e
gnithin <nithin.linkin@gmail.com>
2016-09-30 11:39:26 +0530

    [python] Add the email Ids to the long and medium users

--- python/trials/csv_maker/make_csv.py
+++ python/trials/csv_maker/make_csv.py
@@ -22,6 +22,9 @@ def make_n_entries_for_csv_filename(filename,
     long_interval = num_entries / l_users_num
     med_interval = num_entries / m_users_num
 
+    l_index = 121000
+    m_index = 120500
+
     for i in range(start_id, start_id + num_entries):
         next_str = str(i)
         email_id = "ltuser+" + next_str + "+i@enfold.com"
@@ -29,13 +32,21 @@ def make_n_entries_for_csv_filename(filename,
         output_csv_li.append(",".join([email_id, password]))
 
         if (i-l_starting_index) % long_interval == 0:
-            output_csv_li.append("long")
             long_li.append(i)
+            l_str = str(l_index)
+            email_id = "ltuser+" + l_str + "+l@enfold.com"
+            password = "Enfold*" + l_str
+            output_csv_li.append(",".join([email_id, password]))
             num_long += 1
+            l_index += 1
         if i % med_interval == 0:
-            output_csv_li.append("medium")
             med_li.append(i)
+            m_str = str(m_index)
+            email_id = "ltuser+" + m_str + "+md@enfold.com"
+            password = "Enfold*" + m_str
+            output_csv_li.append(",".join([email_id, password]))
             num_med += 1
+            m_index += 1
 
     with open(filename, 'w') as fw:
         fw.write("\r\n".join(output_csv_li))
